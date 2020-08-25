28187b214ef4e7f90e977bd24c3191b3c05d80f8
Hatim Alattas <56390393+hatimalattas@users.noreply.github.com>
2020-08-25 20:43:47 +0300

    make it better

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 mail = Mail()
-app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_PORT"] = 465
-app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = '7atem96@gmail.com'
-app.config["MAIL_PASSWORD"] = '(1996)Ha'
+# app.config["MAIL_SERVER"] = "smtp.gmail.com"
+# app.config["MAIL_PORT"] = 465
+# app.config["MAIL_USE_SSL"] = True
+# app.config["MAIL_USERNAME"] = '7atem96@gmail.com'
+# app.config["MAIL_PASSWORD"] = '(1996)Ha'
 
 mail.init_app(app)
 
