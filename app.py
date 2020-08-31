4297c2a8ffb2a7397aad7706a882d33a1822400f
Hatim Alattas <56390393+hatimalattas@users.noreply.github.com>
2020-09-01 00:29:56 +0300

    make it better

--- app.py
+++ app.py
@@ -15,17 +15,20 @@ app = Flask(__name__)
 app.config.from_object('config')
 
 # set configuration and instantiate mail
-mail_settings = {
-    "MAIL_SERVER": 'smtp.gmail.com',
-    "MAIL_PORT": 465,
-    "MAIL_USE_TLS": False,
-    "MAIL_USE_SSL": True,
-    "MAIL_USERNAME": os.environ['EMAIL_USER'],
-    "MAIL_PASSWORD": os.environ['EMAIL_PASSWORD']
-}
+# mail_settings = {
+#     "MAIL_SERVER": 'smtp.gmail.com',
+#     "MAIL_PORT": 465,
+#     "MAIL_USE_TLS": False,
+#     "MAIL_USE_SSL": True,
+#     "MAIL_USERNAME": os.environ['EMAIL_USER'],
+#     "MAIL_PASSWORD": os.environ['EMAIL_PASSWORD']
+# }
 
-app.config.update(mail_settings)
-mail = Mail(app)
+# print(os.environ['EMAIL_USER'])
+# print(os.environ['EMAIL_USER'])
+
+# app.config.update(mail_settings)
+# mail = Mail(app)
 
 # mail_settings = {
 #     "MAIL_SERVER": 'smtp.sendgrid.net',
@@ -39,16 +42,16 @@ mail = Mail(app)
 # app.config.update(mail_settings)
 # mail = Mail(app)
 
-# mail_settings = {
-#     "MAIL_SERVER": 'smtp.office365.com',
-#     "MAIL_PORT": 587,
-#     "MAIL_USE_TLS": True,
-#     "MAIL_USE_SSL": False,
-#     "MAIL_USERNAME": os.environ['EMAIL_USER'],
-#     "MAIL_PASSWORD": os.environ['EMAIL_PASSWORD']
-# }
-# app.config.update(mail_settings)
-# mail = Mail(app)
+mail_settings = {
+    "MAIL_SERVER": 'smtp.office365.com',
+    "MAIL_PORT": 587,
+    "MAIL_USE_TLS": True,
+    "MAIL_USE_SSL": False,
+    "MAIL_USERNAME": os.environ['EMAIL_USER'],
+    "MAIL_PASSWORD": os.environ['EMAIL_PASSWORD']
+}
+app.config.update(mail_settings)
+mail = Mail(app)
 
 # mail = Mail()
 # app.config["MAIL_SERVER"] = "smtp-mail.outlook.com"
