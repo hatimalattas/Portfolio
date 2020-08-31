aa1d1239f28343f88c72bf1d0421249fb6ea59f1
Hatim Alattas <56390393+hatimalattas@users.noreply.github.com>
2020-09-01 01:44:09 +0300

    make it better

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from forms import ContactForm
 from flask_mail import Mail, Message
-# from sendgrid import SendGridAPIClient
-# from sendgrid.helpers.mail import Mail
-
 
 ##########################
 #### APP CONFIG. ######
@@ -15,32 +12,6 @@ app = Flask(__name__)
 app.config.from_object('config')
 
 # set configuration and instantiate mail
-# mail_settings = {
-#     "MAIL_SERVER": 'smtp.gmail.com',
-#     "MAIL_PORT": 465,
-#     "MAIL_USE_TLS": False,
-#     "MAIL_USE_SSL": True,
-#     "MAIL_USERNAME": os.environ['EMAIL_USER'],
-#     "MAIL_PASSWORD": os.environ['EMAIL_PASSWORD']
-# }
-
-# print(os.environ['EMAIL_USER'])
-# print(os.environ['EMAIL_USER'])
-
-# app.config.update(mail_settings)
-# mail = Mail(app)
-
-# mail_settings = {
-#     "MAIL_SERVER": 'smtp.sendgrid.net',
-#     "MAIL_PORT": 465,
-#     "MAIL_USERNAME": os.environ['EMAIL_USER'],
-#     "MAIL_PASSWORD": os.environ['EMAIL_PASSWORD'],
-#     "USERNAME": 'apikey',
-#     "PASSWORD": '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'
-# }
-
-# app.config.update(mail_settings)
-# mail = Mail(app)
 
 mail_settings = {
     "MAIL_SERVER": 'smtp.office365.com',
@@ -52,27 +23,6 @@ mail_settings = {
 }
 app.config.update(mail_settings)
 mail = Mail(app)
-
-# mail = Mail()
-# app.config["MAIL_SERVER"] = "smtp-mail.outlook.com"
-# app.config["MAIL_PORT"] = 465
-# app.config["MAIL_USE_SSL"] = True
-# app.config["MAIL_USERNAME"] = 'alattas96@outlook.com'
-# app.config["MAIL_PASSWORD"] = '1996Ha()'
-
-# mail.init_app(app)
-
-# app.config.update(dict(
-#     DEBUG = True,
-#     MAIL_SERVER = 'smtp.gmail.com',
-#     MAIL_PORT = 587,
-#     MAIL_USE_TLS = True,
-#     MAIL_USE_SSL = False,
-#     MAIL_USERNAME = '7atem96@gmail.com',
-#     MAIL_PASSWORD = '(1996)Ha',
-# ))
-
-# mail = Mail(app)
 
 ##########################
 ####### ROUTES ###########
@@ -90,8 +40,11 @@ def index():
         else:
             msg = Message(subject="Hello",
                         sender=app.config.get("MAIL_USERNAME"),
-                        recipients=["alattas96@outlook.com"], # replace with your email for testing
-                        body="This is a test email I sent with Gmail and Python!")
+                        recipients=["alattas96@outlook.com"],
+                        body="""
+                        From: %s %s <%s>
+                        %s
+                        """ % (form.name.data, form.phone.data, form.email.data, form.message.data))
 
             mail.send(msg)
             flash('Thank you for contacting me, your message has been successfully sent to my mailbox.', 'success')
@@ -107,20 +60,3 @@ def download_file():
 
 if __name__ == '__main__':
     app.run()
-    
-
-
-        # else:
-        #   msg = Message('Message from my portfolio website', sender='alattas96@outlook.com', recipients=['7atem96@gmail.com'])
-        #   msg.body = """
-        #   From: %s %s <%s>
-        #   %s
-        #   """ % (form.name.data, form.phone.data, form.email.data, form.message.data)
-        #   mail.send(msg)
-
-            #         msg = Message(subject="Hello",
-            #             sender=app.config.get("MAIL_USERNAME"),
-            #             recipients=["alattas96@outlook.com"], # replace with your email for testing
-            #             body="This is a test email I sent with Gmail and Python!")
-
-            # mail.send(msg)
