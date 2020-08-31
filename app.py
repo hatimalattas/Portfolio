87a56777277ccd6bd7a5cfe8e3618eb2c9638179
Hatim Alattas <56390393+hatimalattas@users.noreply.github.com>
2020-08-31 16:03:19 +0300

    make it better

--- app.py
+++ app.py
@@ -13,16 +13,33 @@ app = Flask(__name__)
 app.config.from_object('config')
 
 # set configuration and instantiate mail
+
+
 mail_settings = {
-    "MAIL_SERVER": 'smtp.office365.com',
-    "MAIL_PORT": 587,
-    "MAIL_USE_TLS": True,
-    "MAIL_USE_SSL": False,
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
     "MAIL_USERNAME": os.environ['EMAIL_USER'],
     "MAIL_PASSWORD": os.environ['EMAIL_PASSWORD']
 }
+
+print(os.environ['EMAIL_USER'])
+print(os.environ['EMAIL_PASSWORD'])
+
 app.config.update(mail_settings)
 mail = Mail(app)
+
+# mail_settings = {
+#     "MAIL_SERVER": 'smtp.office365.com',
+#     "MAIL_PORT": 587,
+#     "MAIL_USE_TLS": True,
+#     "MAIL_USE_SSL": False,
+#     "MAIL_USERNAME": os.environ['EMAIL_USER'],
+#     "MAIL_PASSWORD": os.environ['EMAIL_PASSWORD']
+# }
+# app.config.update(mail_settings)
+# mail = Mail(app)
 
 # mail = Mail()
 # app.config["MAIL_SERVER"] = "smtp-mail.outlook.com"
@@ -59,14 +76,14 @@ def index():
             return render_template('index.html', form=form)
 
         else:
-          msg = Message('Message from my portfolio website', sender='alattas96@outlook.com', recipients=['7atem96@gmail.com'])
-          msg.body = """
-          From: %s %s <%s>
-          %s
-          """ % (form.name.data, form.phone.data, form.email.data, form.message.data)
-          mail.send(msg)
-          flash('Thank you for contacting me, your message has been successfully sent to my mailbox.', 'success')
-          return render_template('index.html', form=form)
+            msg = Message(subject="Hello",
+                        sender=app.config.get("MAIL_USERNAME"),
+                        recipients=["alattas96@outlook.com"], # replace with your email for testing
+                        body="This is a test email I sent with Gmail and Python!")
+
+            mail.send(msg)
+            flash('Thank you for contacting me, your message has been successfully sent to my mailbox.', 'success')
+            return render_template('index.html', form=form)
 
     elif request.method == 'GET':
         return render_template('index.html', form=form)
@@ -79,3 +96,12 @@ def download_file():
 if __name__ == '__main__':
     app.run()
     
+
+
+        # else:
+        #   msg = Message('Message from my portfolio website', sender='alattas96@outlook.com', recipients=['7atem96@gmail.com'])
+        #   msg.body = """
+        #   From: %s %s <%s>
+        #   %s
+        #   """ % (form.name.data, form.phone.data, form.email.data, form.message.data)
+        #   mail.send(msg)
